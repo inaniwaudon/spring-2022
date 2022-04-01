582d60f63f14660c03499a41a04bdfcd18b01297
inaniwaudon <w.yuto0916@gmail.com>
2022-04-02 06:41:19 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,115 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// lookup table
+var gamma = 1.2;
+var correctGamma = function (x) { return Math.pow((x / 255), (1 * gamma)) * 255; };
+var gammaTable = __spreadArray([], Array(255), true).map(function (_, i) { return correctGamma(i); });
+window.onload = function () {
+    // load an image
+    var src = ["cherry1.jpeg"];
+    var image = new Image();
+    image.src = src[Math.round(Math.random() * (src.length - 1))];
+    image.onload = function () {
+        var canvas = document.querySelector("#main");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerWidth * (image.height / image.width);
+        var context = canvas.getContext("2d");
+        // img canvas
+        var imgCanvas = document.createElement("canvas");
+        imgCanvas.width = image.width / 2;
+        imgCanvas.height = image.height / 2;
+        var imgContext = imgCanvas.getContext("2d");
+        imgContext.drawImage(image, 0, 0, imgCanvas.width, imgCanvas.height);
+        var imageData = imgContext.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
+        // bg canvas
+        var bgCanvas = document.createElement("canvas");
+        bgCanvas.width = imgCanvas.width;
+        bgCanvas.height = imgCanvas.height;
+        var bgContext = bgCanvas.getContext("2d");
+        // initialize
+        bgContext.fillStyle = "hsl(204 90% 55%)";
+        bgContext.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
+        var firstBlossoms = [];
+        for (var y = 0; y < imgCanvas.height; y++) {
+            for (var x = 0; x < imgCanvas.width; x++) {
+                var i = (y * imgCanvas.width + x) * 4;
+                var r = gammaTable[imageData.data[i]];
+                var g = gammaTable[imageData.data[i + 1]];
+                var b = gammaTable[imageData.data[i + 2]];
+                // cherry blossoms
+                if (r + g + b > 255 * 1.7) {
+                    if (Math.random() > 0.85) {
+                        var alpha = 0.3 + Math.random() * 0.7;
+                        firstBlossoms.push({
+                            x: x,
+                            y: y,
+                            scale: Math.random() * 2,
+                            r: r,
+                            g: g,
+                            b: b,
+                            alpha: alpha,
+                            angle: 180 + 20 + Math.random() * 140,
+                            delay: Math.random() * 20 + 4
+                        });
+                    }
+                }
+                // others
+                else {
+                    bgContext.fillStyle = "rgb(".concat(r, ", ").concat(g, ", ").concat(b, ")");
+                    bgContext.fillRect(x, y, 1, 1);
+                }
+            }
+        }
+        // draw
+        var scale = canvas.width / imgCanvas.width;
+        var moveBlossoms = function (t) {
+            return firstBlossoms.map(function (blossom) {
+                var radian = (2 * Math.PI * blossom.angle) / 360;
+                var distance = Math.max(t - blossom.delay, 0) * 20;
+                return __assign(__assign({}, blossom), { x: blossom.x + Math.cos(radian) * distance, y: blossom.y + Math.sin(radian) * distance + Math.pow(distance, 2) * 0.002 });
+            });
+        };
+        var draw = function (blossoms) {
+            context.font = "10px \"Zen Old Mincho\"";
+            context.drawImage(bgCanvas, 0, 0, canvas.width, canvas.height);
+            // cherry blossoms
+            for (var _i = 0, blossoms_1 = blossoms; _i < blossoms_1.length; _i++) {
+                var blossom = blossoms_1[_i];
+                context.fillStyle = "rgba(".concat(blossom.r, ", ").concat(blossom.g, ", ").concat(blossom.b, ", ").concat(blossom.alpha, ")");
+                var randomRange = 20;
+                context.save();
+                context.scale(blossom.scale, blossom.scale);
+                var x = blossom.x * scale + Math.random() * randomRange - randomRange / 2;
+                var y = blossom.y * scale + Math.random() * randomRange - randomRange / 2;
+                context.fillText("桜", x / blossom.scale, y / blossom.scale);
+                context.restore();
+            }
+        };
+        var loop = function (t) {
+            var blossoms = moveBlossoms(t);
+            draw(blossoms);
+            setTimeout(function () {
+                loop(t + 1);
+            }, 300);
+        };
+        loop(0);
+    };
+};
---
+++ script.ts
@@ -0,0 +1,130 @@
+// lookup table
+const gamma = 1.2;
+const correctGamma = (x) => (x / 255) ** (1 * gamma) * 255;
+const gammaTable = [...Array(255)].map((_, i) => correctGamma(i));
+
+interface Blossom {
+  x: number;
+  y: number;
+  scale: number;
+  r: number;
+  g: number;
+  b: number;
+  alpha: number;
+  angle: number;
+  delay: number;
+}
+
+window.onload = () => {
+  // load an image
+  const src = ["cherry1.jpeg"];
+  const image = new Image();
+  image.src = src[Math.round(Math.random() * (src.length - 1))];
+
+  image.onload = () => {
+    const canvas = document.querySelector("#main") as HTMLCanvasElement;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerWidth * (image.height / image.width);
+    const context = canvas.getContext("2d");
+
+    // img canvas
+    const imgCanvas = document.createElement("canvas");
+    imgCanvas.width = image.width / 2;
+    imgCanvas.height = image.height / 2;
+    const imgContext = imgCanvas.getContext("2d");
+    imgContext.drawImage(image, 0, 0, imgCanvas.width, imgCanvas.height);
+    const imageData = imgContext.getImageData(
+      0,
+      0,
+      imgCanvas.width,
+      imgCanvas.height
+    );
+
+    // bg canvas
+    const bgCanvas = document.createElement("canvas");
+    bgCanvas.width = imgCanvas.width;
+    bgCanvas.height = imgCanvas.height;
+    const bgContext = bgCanvas.getContext("2d");
+
+    // initialize
+    bgContext.fillStyle = "hsl(204 90% 55%)";
+    bgContext.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
+    const firstBlossoms: Blossom[] = [];
+
+    for (let y = 0; y < imgCanvas.height; y++) {
+      for (let x = 0; x < imgCanvas.width; x++) {
+        const i = (y * imgCanvas.width + x) * 4;
+        const r = gammaTable[imageData.data[i]];
+        const g = gammaTable[imageData.data[i + 1]];
+        const b = gammaTable[imageData.data[i + 2]];
+
+        // cherry blossoms
+        if (r + g + b > 255 * 1.7) {
+          if (Math.random() > 0.85) {
+            const alpha = 0.3 + Math.random() * 0.7;
+            firstBlossoms.push({
+              x,
+              y,
+              scale: Math.random() * 2,
+              r,
+              g,
+              b,
+              alpha,
+              angle: 180 + 20 + Math.random() * 140,
+              delay: Math.random() * 20 + 4,
+            });
+          }
+        }
+
+        // others
+        else {
+          bgContext.fillStyle = `rgb(${r}, ${g}, ${b})`;
+          bgContext.fillRect(x, y, 1, 1);
+        }
+      }
+    }
+
+    // draw
+    const scale = canvas.width / imgCanvas.width;
+
+    const moveBlossoms = (t: number) =>
+      firstBlossoms.map((blossom) => {
+        const radian = (2 * Math.PI * blossom.angle) / 360;
+        const distance = Math.max(t - blossom.delay, 0) * 20;
+        return {
+          ...blossom,
+          x: blossom.x + Math.cos(radian) * distance,
+          y: blossom.y + Math.sin(radian) * distance + distance ** 2 * 0.002,
+        };
+      });
+
+    const draw = (blossoms: Blossom[]) => {
+      context.font = `10px "Zen Old Mincho"`;
+      context.drawImage(bgCanvas, 0, 0, canvas.width, canvas.height);
+
+      // cherry blossoms
+      for (const blossom of blossoms) {
+        context.fillStyle = `rgba(${blossom.r}, ${blossom.g}, ${blossom.b}, ${blossom.alpha})`;
+        const randomRange = 20;
+        context.save();
+        context.scale(blossom.scale, blossom.scale);
+
+        const x =
+          blossom.x * scale + Math.random() * randomRange - randomRange / 2;
+        const y =
+          blossom.y * scale + Math.random() * randomRange - randomRange / 2;
+        context.fillText("桜", x / blossom.scale, y / blossom.scale);
+        context.restore();
+      }
+    };
+
+    const loop = (t: number) => {
+      const blossoms = moveBlossoms(t);
+      draw(blossoms);
+      setTimeout(() => {
+        loop(t + 1);
+      }, 300);
+    };
+    loop(0);
+  };
+};
